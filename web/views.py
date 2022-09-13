1c470859917fe643512675ef499f823002c6e810
mbnahmadi <mbn.ahmadi78@gmail.com>
2022-09-13 12:53:31 +0430

    send request and show data

--- web/views.py
+++ web/views.py
@@ -1,3 +1,17 @@
+from urllib import response
 from django.shortcuts import render
-
+import requests
+from django.http import HttpResponse
+# import json
 # Create your views here.
+
+
+
+
+def show(request):
+    response = requests.get('http://127.0.0.1:8000/api/1')
+    r = response.json()
+    html = '<html><body>%s</body></html>' %r
+    return HttpResponse(html)
+    
+
